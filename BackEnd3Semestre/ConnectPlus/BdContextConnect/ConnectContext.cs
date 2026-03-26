de0e4b436d41f95c6019405e56a2058db83d3c96
MURILO PEREIRA DA SILVA <47649489825@info.local>
2026-03-26 12:56:55 -0300

    API Finalizada

---
+++ BackEnd3Semestre/ConnectPlus/BdContextConnect/ConnectContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using ConnectPlus.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectPlus.BdContextConnect;
+
+public partial class ConnectContext : DbContext
+{
+    public ConnectContext()
+    {
+    }
+
+    public ConnectContext(DbContextOptions<ConnectContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contato> Contatos { get; set; }
+
+    public virtual DbSet<TipoContato> TipoContatos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ConnectPlus;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contato>(entity =>
+        {
+            entity.HasKey(e => e.IdContato).HasName("PK__Contato__2AC4F064837678F3");
+
+            entity.Property(e => e.IdContato).HasDefaultValueSql("(newid())");
+
+            entity.HasOne(d => d.IdTipoContatoNavigation).WithMany(p => p.Contatos).HasConstraintName("FK__Contato__IdTipoC__619B8048");
+        });
+
+        modelBuilder.Entity<TipoContato>(entity =>
+        {
+            entity.HasKey(e => e.IdTipoContato).HasName("PK__TipoCont__8D18FEBDA58219D3");
+
+            entity.Property(e => e.IdTipoContato).HasDefaultValueSql("(newid())");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BackEnd3Semestre/ConnectPlus/Models/Contato.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectPlus.Models;
+
+[Table("Contato")]
+[Index("FormaDeContato", Name = "UQ__Contato__43F549434C23C60F", IsUnique = true)]
+public partial class Contato
+{
+    [Key]
+    public Guid IdContato { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Nome { get; set; } = null!;
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string FormaDeContato { get; set; } = null!;
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? Imagem { get; set; }
+
+    public Guid? IdTipoContato { get; set; }
+
+    [ForeignKey("IdTipoContato")]
+    [InverseProperty("Contatos")]
+    public virtual TipoContato? IdTipoContatoNavigation { get; set; }
+}
